4ea8704b511412c147facdd72202e7c7222ba049
Sarthak <gpt.sarthak1998@gmail.com>
2025-10-12 16:54:06 +0530

    dummy run

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ def main():
     dt_model = DecisionTreeRegressor(random_state=42)
     dt_model = train_model(dt_model, X_train, y_train)
     mse = evaluate_model(dt_model, X_test, y_test)
-
+    print(1)
     print(f"Decision Tree Regressor - Average MSE: {mse:.2f}")
 
 if __name__ == "__main__":
